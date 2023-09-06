08a8d120ff39b502405f9b6e3c6efcb2fa4dea8b
Adeshina Adenuga <Nuga0909@gmail.com>
2023-09-06 18:15:58 +0100

    adjust app.js file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>User List App</h1>
+        <h1>My User List App</h1>
         <UserList />
       </header>
     </div>
